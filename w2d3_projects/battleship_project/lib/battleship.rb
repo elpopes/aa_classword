e70d1ab481e45493134f1b79b8609d4381b533ab
lorenzo <lorenzotijerina@gmail.com>
2022-09-21 17:31:37 -0400

    battleship pass spec yay

--- w2d3_projects/battleship_project/lib/battleship.rb
+++ w2d3_projects/battleship_project/lib/battleship.rb
@@ -2,5 +2,51 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+attr_reader :player,:board
+    def initialize(n)
+        @board = Board.new(n)
+        @player = Player.new
+        @remaining_misses = @board.size / 2
+    end
+
+    def start_game
+        @board.place_random_ships
+        puts @board.num_ships
+        @board.print
+    end
+
+    def lose?
+        if @remaining_misses <= 0
+            puts 'you lose'
+            return true
+        else return false
+        end
+    end
+
+    def win?
+        if @board.num_ships < 1
+            puts 'you win'
+            return true
+        else false
+
+        end
+    end
+
+    def game_over?
+        if self.lose? || self.win?
+            return true
+        else false
+        end        
+    end
+
+    def turn 
+       if @board.attack(@player.get_move) == false
+            @remaining_misses -= 1
+       end
+       @board.print
+       puts @remaining_misses
+    end
+
+
 
 end
--- w2d3_projects/battleship_project/lib/player.rb
+++ w2d3_projects/battleship_project/lib/player.rb
@@ -1,3 +1,9 @@
 class Player
+    def get_move
+        puts 'enter a position with coordinates separated with a space like `4 7`'
+        move = gets.chomp.split(" ")
+        move.map!(&:to_i)
+    end
+
 
 end
